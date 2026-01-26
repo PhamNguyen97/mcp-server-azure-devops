cd07800ec2f639407484b89319362753c85361c9
Phạm Quốc Nguyên <nguyenpq@tmtco.asia>
2026-01-26 09:46:34 +0700

    feat: update commitlint configuration to disable line length limits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,8 @@
 // commitlint.config.js
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [0], // disable the 100 char limit
+    'body-max-line-length': [0], // disable body line length limit
+  },
 }; 
